58b5c88c426b48c60bf08a843c75c133a58d6933
yagobonardi <yagobonardi@gmail.com>
2021-01-28 21:18:10 -0300

    first commit

---
+++ winbarpomodoro/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace winbarpomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseDoubleClick);
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseDoubleClick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 69);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(135, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(154, 69);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(125, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "rest";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "start a cycle!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 105);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "winBarPomodoro";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ winbarpomodoro/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace winbarpomodoro
+{
+    public partial class Form1 : Form
+    {
+        private DateTime StartHour { get; set; }
+        private DateTime StoptHour { get; set; }
+
+        private int Cycle { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Cycle = 0;
+
+            btnStop.Enabled = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+
+            Cycle++;
+
+            notifyIcon.Icon = Properties.Resources.red;
+            notifyIcon.Text = "stay focused!";
+
+            this.WindowState = FormWindowState.Minimized;
+
+            StartHour = DateTime.Now;
+
+            StoptHour = StartHour.AddMinutes(25);
+
+            while (DateTime.Now <= StoptHour) ;
+
+            btnStop.Enabled = true;
+
+            notifyIcon.Icon = Properties.Resources.green;
+            notifyIcon.Text = "cycle ended";
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            SetMessage();
+
+            this.Show();
+
+            this.WindowState = FormWindowState.Normal;
+
+            this.notifyIcon.Visible = false;
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = Properties.Resources.blue;
+            notifyIcon.Text = "just rest";
+
+            this.WindowState = FormWindowState.Minimized;
+
+            var restStart = DateTime.Now;
+
+            var minutesToAdd = 0;
+
+            if (Cycle == 4) { Cycle = 0; minutesToAdd = 15; } else { minutesToAdd = 5; }
+
+            var restStop = restStart.AddMinutes(minutesToAdd);
+
+            while (DateTime.Now < restStop) ;
+
+            label1.Text = "start a cycle!";
+
+            this.Show();
+
+            this.WindowState = FormWindowState.Normal;
+
+            btnStart.Enabled = true;
+
+            btnStop.Enabled = false;
+        }
+
+        private void SetMessage()
+        {
+            switch (Cycle)
+            {
+                case 1:
+                    label1.Text = "First cycle concluded, take five minutes of rest";
+                    break;
+
+                case 2:
+                    label1.Text = "Second cycle concluded, take five minutes of rest";
+                    break;
+
+                case 3:
+                    label1.Text = "Third cycle concluded, take five minutes of rest";
+                    break;
+
+                case 4:
+                    label1.Text = "Fourth cycle concluded! take 15~30 min of rest";
+                    break;
+
+                default:
+                    label1.Text = "start a cycle!";
+                    break;
+            }
+        }
+    }
+}
